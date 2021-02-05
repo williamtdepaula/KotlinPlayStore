42fe932d98144c9014788391ec0274a98fff9d2a
William Tristão de Paula <william@grupowi.com.br>
2021-02-04 22:21:05 -0300

    ::Implemented:: Header and bottom finalized

---
+++ app/src/main/java/com/williamtdepaula/kotlinplaystore/view/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.williamtdepaula.kotlinplaystore.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.williamtdepaula.kotlinplaystore.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, MainFragment())
+                .commitNow()
+        }
+
+    }
+
+
+
+}
